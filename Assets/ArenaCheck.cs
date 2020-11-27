358654e5dfe404e56d5afe73995467f3f468abb3
Cosmin Apopei <cosmin.apopei1702@gmail.com>
2020-11-27 19:05:13 +0000

    Added better points system, added position / rotation reset at round end

--- Assets/ArenaCheck.cs
+++ Assets/ArenaCheck.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class ArenaCheck : MonoBehaviour
 {
-    public PointsManager ptm;
+
     public GameObject arena;
+    public EnvironmentManager evm;
 
     void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject == arena) //if player is exiting collision with arena then
         {
-            ptm.addOppositePoints(this.gameObject); //give points to players still in it
+            evm.EndRound();
         }
     }
 }
