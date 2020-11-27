4dd61362eb686993e9fa9136b41ca79cedb2a8bf
Cosmin Apopei <cosmin.apopei1702@gmail.com>
2020-11-27 18:39:58 +0000

    Added sumo arena system

---
+++ Assets/ArenaCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ArenaCheck : MonoBehaviour
+{
+    public PointsManager ptm;
+    public GameObject arena;
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject == arena) //if player is exiting collision with arena then
+        {
+            ptm.addOppositePoints(this.gameObject); //give points to players still in it
+        }
+    }
+}
